7ba0fce6a4d823d9998f8986661cc9a2294fa283
Your Name <ladraniful10@gmail.com>
2022-12-22 18:36:53 +0100

    finish the home section

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Home = () => {
 
   return (
+    <section>
     <div className='w-full h-[100vh] bg-green-300 relative'>
       <img className='w-full h-full' src={require("../assets/background home.png")} alt="background image" />
       <div className='w-full h-full bg-[#000c] absolute top-0 left-0 flex'>
@@ -21,6 +22,40 @@ const Home = () => {
           </div>
       </div>
     </div>
+    <div className='w-full bg-black'>
+      <div className='container m-auto grid grid-cols-12'>
+          <div className="col-span-6 text-white">
+            <h1 className=' max-w-[518px] h-[126px] text-[48px] font-bold mt-[50px] text-start'>Why to choose Edumena?</h1>
+            <p className='text-[15px] text-start mt-[38px] max-w-[518px]'> We also ensure that our courses are affordable to anyone and accessible from anywhere in the world. Our exceptional mentors are there to guide our students in pursuit of their life aspirations. Together we build a community that supports each other and make the whole experience into another level.</p>
+            <div className='mt-[57px] grid grid-cols-12'>
+                <div className='col-span-6 flex items-center justify-start mb-3'>
+                  <img className='mx-2' src={require("../assets/Vector.png")} alt="icon" />
+                  <span className='text-[18px]'>Watch on any device</span>
+                </div>
+                <div className='col-span-6 flex items-center justify-start mb-3'>
+                  <img className='mx-2' src={require("../assets/Vector.png")} alt="icon" />
+                  <span className='text-[18px]'>Progress 24/7</span>
+                </div>
+                <div className='col-span-6 flex items-center justify-start mb-3'>
+                  <img className='mx-2' src={require("../assets/Vector.png")} alt="icon" />
+                  <span className='text-[18px]'>New courses each month</span>
+                </div>
+                <div className='col-span-6 flex items-center justify-start mb-3'>
+                  <img className='mx-2' src={require("../assets/Vector.png")} alt="icon" />
+                  <span className='text-[18px]'>Multilingual platform</span>
+                </div>
+              <button className='bg-[#F31D1D] rounded-xl rounded-br-xl w-[257px] h-[54px] hover:bg-[#f31d1dad] mt-[74px] mb-[92px]'>Discover all classes</button>
+            </div>
+          </div>
+          <div className="col-span-6 flex items-center justify-end">
+            <div className='relative w-[447px] h-[550px] '>
+            <img className='absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-10' src={require("../assets/image.png")} alt="img" />
+            <span className='absolute bg-[#F31D1D] w-[487px] h-[415px] rounded-xl right-1 bottom-0 '></span>
+            </div>
+          </div>
+      </div>
+    </div>
+    </section>
   )
 }
 
